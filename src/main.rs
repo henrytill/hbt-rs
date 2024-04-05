8192a83d4f647839154708524b94d53a5190b223
Henry Till <henrytill@gmail.com>
2024-04-05 00:38:10 -0700

    remove anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::{env, fs};
-
-use anyhow::Result;
+use std::{env, fs, process::ExitCode};
 
 use hbt::markdown;
 
@@ -19,7 +17,7 @@ impl std::fmt::Display for Error {
 
 impl std::error::Error for Error {}
 
-fn main() -> Result<()> {
+fn run() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         let exe = args[0].to_owned();
@@ -30,4 +28,14 @@ fn main() -> Result<()> {
     let collection = markdown::parse(&contents)?;
     println!("collection.len(): {}", collection.len());
     Ok(())
+}
+
+fn main() -> ExitCode {
+    match run() {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            ExitCode::FAILURE
+        }
+    }
 }
