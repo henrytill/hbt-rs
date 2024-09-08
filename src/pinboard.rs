5bc1084f9e31bf5a0eca2ce20cab3b1aaaf574b2
Henry Till <henrytill@gmail.com>
2024-09-08 00:19:12 -0700

    pinboard: restructure

--- src/pinboard.rs
+++ src/pinboard.rs
@@ -4,15 +4,7 @@ mod tests;
 use std::collections::{hash_set::Iter, HashSet};
 
 use anyhow::Error;
-use quick_xml::{
-    events::{
-        attributes::{Attribute, Attributes},
-        Event,
-    },
-    reader::Reader,
-};
-use scraper::{Element, Html, Selector};
-use serde::{Deserialize, Deserializer};
+use serde::Deserialize;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Tags(HashSet<String>);
@@ -44,17 +36,17 @@ impl Tags {
 pub struct Post {
     href: String,
     time: String,
-    #[serde(deserialize_with = "deserialize_empty_string")]
+    #[serde(deserialize_with = "json::deserialize_empty_string")]
     description: Option<String>,
-    #[serde(deserialize_with = "deserialize_empty_string")]
+    #[serde(deserialize_with = "json::deserialize_empty_string")]
     extended: Option<String>,
-    #[serde(deserialize_with = "deserialize_tags", default)]
+    #[serde(deserialize_with = "json::deserialize_tags", default)]
     tags: Vec<String>,
-    #[serde(deserialize_with = "deserialize_empty_string")]
+    #[serde(deserialize_with = "json::deserialize_empty_string")]
     hash: Option<String>,
-    #[serde(deserialize_with = "deserialize_yes_no")]
+    #[serde(deserialize_with = "json::deserialize_yes_no")]
     shared: bool,
-    #[serde(deserialize_with = "deserialize_yes_no")]
+    #[serde(deserialize_with = "json::deserialize_yes_no")]
     toread: bool,
 }
 
@@ -75,12 +67,45 @@ impl Post {
     }
 
     pub fn from_html(input: &str) -> Result<Vec<Post>, Error> {
+        html::parse(input)
+    }
+
+    pub fn from_json(input: &str) -> Result<Vec<Post>, Error> {
+        serde_json::from_str(input).map_err(Into::into)
+    }
+
+    pub fn from_xml(input: &str) -> Result<Vec<Post>, Error> {
+        xml::parse(input)
+    }
+
+    pub fn tags(ps: &[Post]) -> Tags {
+        let mut inner = HashSet::new();
+        for p in ps {
+            inner.extend(p.tags.iter().cloned());
+        }
+        Tags(inner)
+    }
+
+    pub fn href(&self) -> &String {
+        &self.href
+    }
+}
+
+mod html {
+    use anyhow::Error;
+    use scraper::{Element, Html, Selector};
+
+    use super::Post;
+
+    #[inline]
+    pub fn parse(input: &str) -> Result<Vec<Post>, Error> {
         let document = Html::parse_document(input);
         let dt_selector = Selector::parse("dt").unwrap();
 
         let posts = document
             .select(&dt_selector)
             .filter_map(|dt_element| {
+                // NOTE: These question marks are for Option
                 let a_selector = Selector::parse("a").ok()?;
                 let a_element = dt_element.select(&a_selector).next()?;
 
@@ -119,13 +144,115 @@ impl Post {
 
         Ok(posts)
     }
+}
 
-    pub fn from_json(input: &str) -> Result<Vec<Post>, Error> {
-        let posts: Vec<Post> = serde_json::from_str(input)?;
-        Ok(posts)
+mod json {
+    use serde::{Deserialize, Deserializer};
+
+    pub fn deserialize_empty_string<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        if s.is_empty() {
+            Ok(None)
+        } else {
+            Ok(Some(s))
+        }
     }
 
-    pub fn from_xml(input: &str) -> Result<Vec<Post>, Error> {
+    pub fn deserialize_tags<'de, D>(deserializer: D) -> Result<Vec<String>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        if s.is_empty() {
+            Ok(Vec::new())
+        } else {
+            Ok(s.split_whitespace().map(ToOwned::to_owned).collect())
+        }
+    }
+
+    pub fn deserialize_yes_no<'de, D>(deserializer: D) -> Result<bool, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        match s.to_lowercase().as_str() {
+            "yes" => Ok(true),
+            "no" => Ok(false),
+            _ => Err(serde::de::Error::custom("expected 'yes' or 'no'")),
+        }
+    }
+}
+
+mod xml {
+    use anyhow::Error;
+    use quick_xml::{
+        events::{
+            attributes::{Attribute, Attributes},
+            Event,
+        },
+        reader::Reader,
+    };
+
+    use super::Post;
+
+    pub fn extract_post(attrs: Attributes) -> Result<Post, Error> {
+        let mut ret = Post::default();
+
+        for attr in attrs {
+            let Attribute { key, value } = attr?;
+            match key.local_name().as_ref() {
+                b"href" => ret.href = String::from_utf8(value.into_owned())?,
+                b"time" => ret.time = String::from_utf8(value.into_owned())?,
+                b"description" => {
+                    ret.description = if value.is_empty() {
+                        None
+                    } else {
+                        let s = String::from_utf8(value.into_owned())?;
+                        Some(s)
+                    };
+                }
+                b"extended" => {
+                    ret.extended = if value.is_empty() {
+                        None
+                    } else {
+                        let s = String::from_utf8(value.into_owned())?;
+                        Some(s)
+                    };
+                }
+                b"tag" => {
+                    ret.tags = if value.is_empty() {
+                        Vec::new()
+                    } else {
+                        let s = String::from_utf8(value.into_owned())?;
+                        s.split_whitespace().map(ToOwned::to_owned).collect()
+                    }
+                }
+                b"hash" => {
+                    ret.hash = if value.is_empty() {
+                        None
+                    } else {
+                        let s = String::from_utf8(value.into_owned())?;
+                        Some(s)
+                    };
+                }
+                b"shared" => {
+                    ret.shared = value.as_ref() == b"yes";
+                }
+                b"toread" => {
+                    ret.toread = value.as_ref() == b"yes";
+                }
+                _ => (),
+            }
+        }
+
+        Ok(ret)
+    }
+
+    #[inline]
+    pub fn parse(input: &str) -> Result<Vec<Post>, Error> {
         let mut ret = Vec::new();
         let mut reader = Reader::from_str(input);
         reader.config_mut().trim_text(true);
@@ -146,105 +273,4 @@ impl Post {
 
         Ok(ret)
     }
-
-    pub fn tags(ps: &[Post]) -> Tags {
-        let mut inner = HashSet::new();
-        for p in ps {
-            inner.extend(p.tags.iter().cloned());
-        }
-        Tags(inner)
-    }
-
-    pub fn href(&self) -> &String {
-        &self.href
-    }
-}
-
-fn deserialize_empty_string<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let s = String::deserialize(deserializer)?;
-    if s.is_empty() {
-        Ok(None)
-    } else {
-        Ok(Some(s))
-    }
-}
-
-fn deserialize_tags<'de, D>(deserializer: D) -> Result<Vec<String>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let s = String::deserialize(deserializer)?;
-    if s.is_empty() {
-        Ok(Vec::new())
-    } else {
-        Ok(s.split_whitespace().map(ToOwned::to_owned).collect())
-    }
-}
-
-fn deserialize_yes_no<'de, D>(deserializer: D) -> Result<bool, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let s = String::deserialize(deserializer)?;
-    match s.to_lowercase().as_str() {
-        "yes" => Ok(true),
-        "no" => Ok(false),
-        _ => Err(serde::de::Error::custom("expected 'yes' or 'no'")),
-    }
-}
-
-fn extract_post(attrs: Attributes) -> Result<Post, Error> {
-    let mut ret = Post::default();
-
-    for attr in attrs {
-        let Attribute { key, value } = attr?;
-        match key.local_name().as_ref() {
-            b"href" => ret.href = String::from_utf8(value.into_owned())?,
-            b"time" => ret.time = String::from_utf8(value.into_owned())?,
-            b"description" => {
-                ret.description = if value.is_empty() {
-                    None
-                } else {
-                    let s = String::from_utf8(value.into_owned())?;
-                    Some(s)
-                };
-            }
-            b"extended" => {
-                ret.extended = if value.is_empty() {
-                    None
-                } else {
-                    let s = String::from_utf8(value.into_owned())?;
-                    Some(s)
-                };
-            }
-            b"tag" => {
-                ret.tags = if value.is_empty() {
-                    Vec::new()
-                } else {
-                    let s = String::from_utf8(value.into_owned())?;
-                    s.split_whitespace().map(ToOwned::to_owned).collect()
-                }
-            }
-            b"hash" => {
-                ret.hash = if value.is_empty() {
-                    None
-                } else {
-                    let s = String::from_utf8(value.into_owned())?;
-                    Some(s)
-                };
-            }
-            b"shared" => {
-                ret.shared = value.as_ref() == b"yes";
-            }
-            b"toread" => {
-                ret.toread = value.as_ref() == b"yes";
-            }
-            _ => (),
-        }
-    }
-
-    Ok(ret)
 }
