5f426ea16d8c1aa9288d38196a95a1e8a192cd46
Henry Till <henrytill@gmail.com>
2024-10-22 23:44:32 -0700

    pinboard: remove escaping from TEST_JSON_SAMPLE

--- src/pinboard/tests.rs
+++ src/pinboard/tests.rs
@@ -61,9 +61,9 @@ fn test_xml_sample() {
 }
 
 const TEST_JSON_SAMPLE: &str = r#"[
-  {"href":"https:\/\/github.com\/janestreet\/magic-trace","description":"janestreet\/magic-trace: magic-trace collects and displays high-resolution traces of what a process is doing","extended":"tragically intel-only","meta":"54866bdf6b1dcbb915d917f2e2394748","hash":"54dab27be2409c987bb17fc06e47a729","time":"2022-04-23T00:29:36Z","shared":"yes","toread":"no","tags":"performance profiling tools"},
-  {"href":"https:\/\/www.intel.com\/content\/www\/us\/en\/developer\/tools\/oneapi\/vtune-profiler.html#gs.x8oazh","description":"Fix Performance Bottlenecks with Intel\u00ae VTune\u2122 Profiler","extended":"","meta":"2a438b267411603d2077a26862a260e6","hash":"2ab1611711c8bb5ed9273b8f4b612fca","time":"2022-04-13T13:12:10Z","shared":"no","toread":"yes","tags":"performance profiling tools"},
-  {"href":"http:\/\/kcachegrind.sourceforge.net\/html\/Home.html","description":"","extended":"","meta":"a2c175993139aed54ad3ff002439625d","hash":"0850c315d075a67430db267214b18d13","time":"2022-04-13T08:01:32Z","shared":"no","toread":"no","tags":"performance profiling tools"}
+  {"href":"https://github.com/janestreet/magic-trace","description":"janestreet/magic-trace: magic-trace collects and displays high-resolution traces of what a process is doing","extended":"tragically intel-only","meta":"54866bdf6b1dcbb915d917f2e2394748","hash":"54dab27be2409c987bb17fc06e47a729","time":"2022-04-23T00:29:36Z","shared":"yes","toread":"no","tags":"performance profiling tools"},
+  {"href":"https://www.intel.com/content/www/us/en/developer/tools/oneapi/vtune-profiler.html#gs.x8oazh","description":"Fix Performance Bottlenecks with Intel\u00ae VTune\u2122 Profiler","extended":"","meta":"2a438b267411603d2077a26862a260e6","hash":"2ab1611711c8bb5ed9273b8f4b612fca","time":"2022-04-13T13:12:10Z","shared":"no","toread":"yes","tags":"performance profiling tools"},
+  {"href":"http://kcachegrind.sourceforge.net/html/Home.html","description":"","extended":"","meta":"a2c175993139aed54ad3ff002439625d","hash":"0850c315d075a67430db267214b18d13","time":"2022-04-13T08:01:32Z","shared":"no","toread":"no","tags":"performance profiling tools"}
 ]
 "#;
 
