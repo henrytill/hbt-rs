52be27fc61e7ee247c6c9658919aa8463df754c1
Henry Till <henrytill@gmail.com>
2025-08-12 18:56:03 -0700

    flake.nix: update nixpkgs input to nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
