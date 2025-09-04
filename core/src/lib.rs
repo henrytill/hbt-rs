319e2c733c45a4a520fe12cee1e4b006af010435
Henry Till <henrytill@gmail.com>
2025-09-03 23:34:18 -0700

    core: remove format re-exports

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,7 +4,3 @@ pub mod format;
 pub mod html;
 pub mod markdown;
 pub mod pinboard;
-
-pub use format::{
-    ALL_INPUT_FORMATS, ALL_OUTPUT_FORMATS, Format, INPUT, OUTPUT, ParseError, UnparseError,
-};
